51863802ae33141212021fa0450492c9326a5cc2
BABAH GEE <61418505+babahgee@users.noreply.github.com>
2025-12-16 15:27:17 +0100

    Can now apply effects on master channel as well.

--- lib/dist/core/classes/Master.d.ts
+++ lib/dist/core/classes/Master.d.ts
@@ -1,11 +1,18 @@
 import { Channel } from "./Channel";
+import { Effector } from "./Effector";
 export declare class Master {
     id: string;
     channels: Channel[];
+    effects: Effector[];
+    inputNode: GainNode | null;
     gainNode: GainNode | null;
     analyserNode: AnalyserNode | null;
     context: AudioContext | null;
     constructor(context: AudioContext);
+    private disconnectAudioNodes;
+    private rebuildEffectChain;
+    AttachEffect(effect: Effector): void;
+    DetachEffect(effect: Effector): void;
     AttachChannel(channel: Channel): void;
     DetachChannel(channel: Channel): void;
 }
--- lib/src/core/classes/Master.ts
+++ lib/src/core/classes/Master.ts
@@ -1,29 +1,96 @@
 import { v4 } from "uuid";
 
 import { Channel } from "./Channel";
-
 import { Debug } from "../../utilities/debugger";
-import { AudioClipPlayer } from "./AudioClipPlayer";
+import { Effector } from "./Effector";
 
 export class Master {
 
     public id: string = v4();
-    public channels: Channel[] = [];
 
+    public channels: Channel[] = [];
+    public effects: Effector[] = [];
+
+    public inputNode: GainNode | null = null;
     public gainNode: GainNode | null = null;
     public analyserNode: AnalyserNode | null = null;
-
     public context: AudioContext | null = null;
 
     constructor(context: AudioContext) {
 
         this.context = context;
 
+        this.inputNode = new GainNode(context);
         this.gainNode = new GainNode(context);
         this.analyserNode = new AnalyserNode(context);
 
+        this.inputNode.connect(this.gainNode);
         this.gainNode.connect(this.analyserNode);
         this.analyserNode.connect(this.context.destination);
+    }
+
+    private disconnectAudioNodes(): void {
+
+        if (this.inputNode) this.inputNode.disconnect();
+        if (this.gainNode) this.gainNode.disconnect();
+        if (this.analyserNode) this.analyserNode.disconnect();
+
+        this.effects.forEach(function (effect: Effector) {
+            if (effect.audioWorkletNode)
+                effect.audioWorkletNode.disconnect();
+        });
+    }
+
+    private rebuildEffectChain(): void {
+
+        if (!this.context) return;
+        if (!this.inputNode || !this.gainNode || !this.analyserNode) return;
+
+        this.disconnectAudioNodes();
+
+        let currentNode: AudioNode = this.inputNode;
+
+        this.effects.forEach(function (effect: Effector) {
+
+            if (!effect.audioWorkletNode) return;
+
+            currentNode.connect(effect.audioWorkletNode);
+            currentNode = effect.audioWorkletNode;
+        });
+
+        currentNode.connect(this.gainNode);
+
+        this.gainNode.connect(this.analyserNode);
+        this.analyserNode.connect(this.context.destination);
+    }
+
+    public AttachEffect(effect: Effector): void {
+
+        if (this.effects.includes(effect)) return Debug.Error("Could not attach the effect because it is already part of this master channel.", [
+            "Call .DetachEffect([effect Effector]) before attaching the effect."
+        ]);
+
+        this.effects.push(effect);
+        this.rebuildEffectChain();
+    }
+
+    public DetachEffect(effect: Effector): void {
+
+        if (!this.effects.includes(effect)) return Debug.Error("Could not detach the effect because it is not part of this master channel.", [
+            "Call .AttachEffect([effect Effector]) before detaching the effect."
+        ]);
+
+        const self = this;
+
+        this.effects.forEach(function (_effect: Effector, index: number) {
+
+            if (effect.id === _effect.id) {
+                self.effects.splice(index, 1);
+                return;
+            }
+        });
+
+        this.rebuildEffectChain();
     }
 
     public AttachChannel(channel: Channel): void {
@@ -34,9 +101,9 @@ export class Master {
 
         this.channels.push(channel);
 
-        if (channel.output && this.gainNode)
-            channel.output.connect(this.gainNode);
-        
+        if (channel.output && this.inputNode)
+            channel.output.connect(this.inputNode);
+
         return;
     }
 
@@ -46,17 +113,15 @@ export class Master {
             "Call .AttachChannel([channel Channel]) before detaching the channel."
         ]);
 
-        if (channel.output && this.gainNode)
-            channel.output.disconnect(this.gainNode);
+        if (channel.output && this.inputNode)
+            channel.output.disconnect(this.inputNode);
 
         const self = this;
 
         this.channels.forEach(function (_channel: Channel, index: number) {
-            if (channel.id === _channel.id) {
-
-                self.channels.splice(index, 1);
-                return;
-            }
+            if (channel.id !== _channel.id) return;
+            
+            self.channels.splice(index, 1);
         });
     }
 }
